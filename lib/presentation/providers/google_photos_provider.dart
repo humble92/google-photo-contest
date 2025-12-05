6ec4bd6d7c6522e4b332769c3707d7b37f7e22b2
Daniel Hwang <humble92@gmail.com>
2025-12-05 18:34:05 -0500

    DEL: googles photos API

--- lib/presentation/providers/google_photos_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:humble_photo_contest/data/services/google_photos_service.dart';
-
-final googlePhotosServiceProvider = Provider<GooglePhotosService>((ref) {
-  return GooglePhotosService();
-});
